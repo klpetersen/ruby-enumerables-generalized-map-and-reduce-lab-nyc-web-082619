86c89f5c35ad3a646c985f09c9b4540b8e7a6b1e
Kayla Petersen <petersen1994@gmail.com>
2019-08-19 17:58:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
-# Your Code Here
+def map(array)
+  i = 0 
+  array2 = []
+  while i < array.length do
+   array2.push(yield(array[i]))
+   i += 1
+  end
+  array2
+end
+
+def reduce(array, starting_value = 0)
+  i = 0 
+  while i < array.length do
+    yield(starting_value += array[i])
+    i += 1
+  end
+  starting_value
+end 
