945f8ecede63aa25fa8cb8649ec369bdd14dd3e9
Kayla Petersen <petersen1994@gmail.com>
2019-08-19 18:06:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,11 +8,18 @@ def map(array)
   array2
 end
 
-def reduce(array, starting_value = 0)
-  i = 0 
-  while i < array.length do
-    yield(starting_value += array[i])
-    i += 1
-  end
-  starting_value
-end 
+def reduce(array, sv = nil)
+    if sv
+      sum = sv
+      i = 0 
+    else 
+      sum = array[0]
+      i = 1
+    end
+    while i < array.length
+      sum = yield(sum, array[i])
+      i += 1
+    end
+    sum 
+end
+  
